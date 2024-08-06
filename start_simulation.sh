6e79680a18388b7a61aa6ec9f25c6316225c2bda
miguelrosasvillalobos <miguel.rosas2001@alumnos.ubiobio.cl>
2024-08-05 21:40:12 -0400

    se corrige error en nombre directorio

--- start_simulation.sh
+++ start_simulation.sh
@@ -49,7 +49,7 @@ for ((i = 1; i <= $cantidad; i++)); do
 	sed -i "s/\$i/$i/g" "$nombre_carpeta/extract_freesurface_plane.py"
 	sed -i "s/\$i/$i/g" "$nombre_carpeta/extractor.py"
 	sed -i "s/\$nn/$n/g" "$nombre_carpeta/constant/porosityProperties"
-	sed -i "s/\$nn/$n/g" "$nombre_carpeta/system/setFields"
+	sed -i "s/\$nn/$n/g" "$nombre_carpeta/system/setFieldsDict"
 
 	#Generar mallado gmsh
 	cd "$nombre_carpeta/"
